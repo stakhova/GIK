bae6066d2a1cfe6cbcf6a8e9a749682422860995
Halyna Dovhopola <stahova2404@gmail.com>
2023-11-23 12:59:23 +0200

    fix

--- src/scripts/develop/index.js
+++ src/scripts/develop/index.js
@@ -1,8 +1,25 @@
 
+function openDrop(){
+    $('.section__drop').click(function (){
+        $(this).next().toggle(300)
+        $(this).toggleClass('section__drop-open')
+    })
+}
+
+const main = new Swiper('.testimonials__slider', {
+    slidesPerView: 1,
+    spaceBetween: 60,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: ".testimonials__next",
+        prevEl: ".testimonials__prev"
+    }
+});
 
 
 $(document).ready(function(){
-
+    openDrop();
 });
 
 $(window).load(function(){
