1a93305288c7c9f545182f9abafd141dfab5c2eb
Halyna Dovhopola <stahova2404@gmail.com>
2023-11-24 15:29:19 +0200

    fix

--- src/scripts/develop/index.js
+++ src/scripts/develop/index.js
@@ -20,18 +20,27 @@ const main = new Swiper('.testimonials__slider', {
 
 
 function showSideMenu(){
-    $(".side__item h2").on("click", function() {
+    $(".side__item > *").on("click", function() {
         let tab = $(this).data('tab-name')
-        $('.side__content-item').hide()
-        $('.side__content-item').each(function (){
+        // $('.side__content').hide()
+        $(this).toggleClass('active')
+        $(this).prev().removeClass('active')
+        $(this).next().removeClass('active')
+        $('.side__content').each(function (){
             let tabContent = $(this).data('name')
+            console.log(12345,tab,tabContent)
             if( tab == tabContent){
-                $(this).css('display','flex')
+                console.log(55555,$(this))
+                $(this).toggleClass('show')
+                $(this).next().removeClass('show')
+                $(this).prev().removeClass('show')
+                $(this).find('.side__content-item').toggleClass('flex')
             }
         })
-        $(".side__content").animate({left: '5.9rem'}, 500);
+        // $('.side__content').fadeOut(500);
+        $('.side__content').animate({left: '-100%'}, 500);
+        $('.side__content.show').animate({left: '5.9rem'}, 500);
     });
-
 }
 $(document).ready(function(){
     openDrop();
