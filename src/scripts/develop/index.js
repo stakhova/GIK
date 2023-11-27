ac5a0d7724bcb6fa951b673bbea7865cb3ca41c8
Halyna Dovhopola <stahova2404@gmail.com>
2023-11-27 17:28:05 +0200

    fix

--- src/scripts/develop/index.js
+++ src/scripts/develop/index.js
@@ -33,15 +33,69 @@ const banner = new Swiper('.banner__slider', {
         prevEl: ".banner__prev"
     }
 });
-function mobChange(){
-    if(window.innerWidth <= 666) {
 
-        $('.header__top-block').prepend($('.header__logo'))
+function dropMenu(){
+    let dropMenu = $('.header__drop-menu .header__drop-btn')
+    let dropSubmenu = ('.header__drop-btn-wrap .header__drop-btn')
+
+    dropMenu.click(function (){
+      let submenu = $(this).closest('.header__drop').find('.header__drop-wrap')
+      $('.header__drop-list').removeClass('show')
+      if($(this).hasClass('header__drop-btn-open')){
+          $(this).removeClass('header__drop-btn-open')
+      } else{
+          $('.header__drop-btn').removeClass('header__drop-btn-open')
+          $(this).addClass('header__drop-btn-open')
+      }
+      if(submenu.hasClass('show')){
+            submenu.removeClass('show')
+      }else{
+          $('.header__drop-wrap').removeClass('show')
+          submenu.addClass('show')
+      }
+    })
+
+    $(document).on('click', dropSubmenu, function (){
+        let submenu = $(this).closest('.header__drop-item').find('.header__drop-list')
+        if($(this).hasClass('header__drop-btn-open')){
+            $(this).removeClass('header__drop-btn-open')
+        } else{
+            $('.header__drop-content .header__drop-btn').removeClass('header__drop-btn-open')
+            $(this).addClass('header__drop-btn-open')
+        }
+        if(submenu.hasClass('show')){
+            submenu.removeClass('show')
+        }else{
+            $('.header__drop-list').removeClass('show')
+            submenu.addClass('show')
+        }
+    })
+}
+
+function openMenu() {
+    $('.header__burger').click(function (){
+        // $(this).toggleClass("header__burger-open");
+        $('.header').toggleClass('header__show');
+        $('body').toggleClass('hidden');
+        $('.header__back').toggleClass('header__back-show')
+    })
+    $('.header__hide').on('click',function (){
+        // $(this).toggleClass("header__burger-open");
+        $('.header').toggleClass('header__show');
+        $('body').toggleClass('hidden');
+        $('.header__back').toggleClass('header__back-show')
+    })
+};
+function mobChange(){
+
+    if(window.innerWidth <= 666) {
+        $('.header__drop-item').each(function (){
+            $(this).append('<div class="header__drop-btn-wrap"><button class="header__drop-btn"></button></div>')
+        })
         $('.header__block').append($('.header__social'))
         $('.delivery__text h2, .delivery__text h3').wrapAll("<div class='delivery__text-block'></div>")
         $(".footer__item:nth-child(4), .footer__item:nth-child(5)").wrapAll("<div class='footer__item footer__item-mob'></div>");
 
-        //feeback-slider init
         const main = new Swiper('.feedback__slider', {
             slidesPerView: 1.5,
             spaceBetween: 20,
@@ -54,38 +108,64 @@ function mobChange(){
         });
         let slidersCount = $('.feedback .swiper-pagination-bullet').length
         $('.feedback .swiper-pagination-bullet').css('width',`calc(100%/${slidersCount})`)
+    } else{
+        $('body').addClass('desc')
     }
 
 
 }
 
 function showSideMenu(){
+
+    let block = $('.side__block')
     $(".side__item > *").on("click", function() {
         let tab = $(this).data('tab-name')
-        // $('.side__content').hide()
+
+        if($(this).hasClass('active')){
+            console.log(111111111111)
+            block.removeClass("show")
+            block.addClass("hide")
+            setTimeout(function(){
+                $('.side__content').removeClass('show')
+            },500)
+        } else {
+            block.removeClass("hide")
+            block.addClass('show')
+            $('.side__content').each(function (){
+                let tabContent = $(this).data('name')
+                if( tab == tabContent){
+                    $(this).toggleClass('show')
+                    $(this).next().removeClass('show')
+                    $(this).prev().removeClass('show')
+                }
+            })
+        }
+
         $(this).toggleClass('active')
         $(this).prev().removeClass('active')
         $(this).next().removeClass('active')
-        $('.side__content').each(function (){
-            let tabContent = $(this).data('name')
-            console.log(12345,tab,tabContent)
-            if( tab == tabContent){
-                console.log(55555,$(this))
-                $(this).toggleClass('show')
-                $(this).next().removeClass('show')
-                $(this).prev().removeClass('show')
-                $(this).find('.side__content-item').toggleClass('flex')
-            }
-        })
-        // $('.side__content').fadeOut(500);
-        $('.side__content').animate({left: '-100%'}, 500);
-        $('.side__content.show').animate({left: '5.9rem'}, 500);
+
+
+        $('.side__block.show').animate({left: '5.9rem'}, 500);
+        $('.side__block.hide').animate({left: '-100%'}, 500);
     });
+
+    $(document).on('click','.side__block-close', function (){
+        block.removeClass("show")
+        block.addClass("hide")
+        $('.side__item > *').removeClass("active")
+        setTimeout(function(){
+            $('.side__content').removeClass('show')
+        },500)
+        $('.side__block.hide').animate({left: '-100%'}, 500);
+    })
 }
 $(document).ready(function(){
     openDrop();
     showSideMenu()
     mobChange()
+    openMenu()
+    dropMenu()
 });
 
 $(window).load(function(){
